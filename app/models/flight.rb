eb06aa20b1ac73522367109576bc8149312b67f4
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-06-11 10:42:50 +0100

    Configured the models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,10 @@
 class Flight < ApplicationRecord
+  belongs_to :departure_airport, :class_name => "Airport"
+  belongs_to :arrival_airport, :class_name => "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
+
+  def date_formatted
+    departure_time.strftime("%m/%d/%Y")
+  end
 end
