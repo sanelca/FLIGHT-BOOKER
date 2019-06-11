dd5c33158cd44aa49f77445bd8da213cf8e98a70
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-11 11:58:52 +0200

    Bookings actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,38 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @num_passengers = params[:num_passengers].to_i
+    if @num_passengers < 1 || @num_passengers > 4
+      @num_passengers = 1
+    end
+    @num_passengers.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:success] = "Booking created!"
+      redirect_to @booking
+    else
+      @flight = Flight.find(booking_params[:flight_id].to_i)
+      if match = /\d/.match(params[:num_passengers])
+        @num_passengers = match.captures.first.to_i
+      end
+      render 'new'
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email, :booking_id])
+    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :bookings, only: [:new, :create, :index, :show]  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
