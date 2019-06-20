29ef6a90b17048235bf28a01d9290117c4b4f3d0
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-06-20 13:43:46 +0200

    fat models tiny controllers

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,17 +4,9 @@ class FlightsController < ApplicationController
     # @flight = Flight.new
     @airports_options = Airport.all.map{|a| [ "#{a.code}, #{a.name}", a.id ]}
     @num_passenger_options = [1,2,3,4].map{|num| [ pluralize(num, 'person'), num ]}
-    @departure_time_options = Flight.select(:departure_time).group('DATE(departure_time)')
-                                    .map{ |f| [ f.date_formatted, f.departure_time.to_date ]}
-
+    @departure_time_options = Flight.departure_time_options
     @num_passengers = params[:num_passengers] unless params[:num_passengers].nil?
-    @queried_flights = []
-    if !params[:departure_airport_id].nil?
-      @queried_flights = Flight.all
-      @queried_flights = @queried_flights.where(departure_airport_id: params[:departure_airport_id])
-      @queried_flights = @queried_flights.where(arrival_airport_id: params[:arrival_airport_id])
-      @queried_flights = @queried_flights.where("DATE(departure_time) = ?", params[:departure_time].to_date)
-    end
+    @queried_flights = Flight.queried_flights(params)
   end
 
   private
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,4 +7,20 @@ class Flight < ApplicationRecord
   def date_formatted
     departure_time.strftime("%m/%d/%Y")
   end
+
+  def self.departure_time_options
+    return Flight.select(:departure_time).group('DATE(departure_time)')
+                                    .map{ |f| [ f.date_formatted, f.departure_time.to_date ]}
+  end
+
+  def self.queried_flights departure_params
+    queried_flights = []
+    if !departure_params[:departure_airport_id].nil?
+      queried_flights = Flight.all
+      queried_flights = queried_flights.where(departure_airport_id: departure_params[:departure_airport_id])
+      queried_flights = queried_flights.where(arrival_airport_id: departure_params[:arrival_airport_id])
+      queried_flights = queried_flights.where("DATE(departure_time) = ?", departure_params[:departure_time].to_date)
+    end
+    return queried_flights
+  end
 end
